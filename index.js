95f6c02952e9de87cf73dae2374c508c2b6903cb
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-11-10 10:14:13 -0500

    dom

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const DOMSelectors = {
   text: document.getElementById("text"),
   box: document.getElementById("container-box"),
   input: document.querySelector("input"),
+  input2: document.querySelector("input2"),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input}</p>`);
   /*DOMSelectors.button.insertAdjacentHTML("afterend", `<p>${input}</p>`);*/
   DOMSelectors.input.value = "";
+});
+console.log(DOMSelectors.input.value);
+
+DOMSelectors.button.addEventListener("click", function () {
+  let input2 = DOMSelectors.input2.value2;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input2}</p>`);
+  /*DOMSelectors.button.insertAdjacentHTML("afterend", `<p>${input}</p>`);*/
+  DOMSelectors.input2.value = "";
 });
