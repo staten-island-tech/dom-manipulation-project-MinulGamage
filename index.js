599362373ab968c4f4a3ea08455f2149e1a9d7f3
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-11-15 10:15:54 -0500

    dom

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ const DOMSelectors = {
   form: document.getElementById("mainForm"),
   Name: document.querySelector(`#Name`),
   DOB: document.querySelector(`#DOB`),
+  Remove: document.getElementById("Remove"),
 };
-
+console.log(DOMSelectors.Name);
+console.log(DOMSelectors.DOB);
 DOMSelectors.button.addEventListener("click", function () {
   let Name = DOMSelectors.Name.value;
   let DOB = DOMSelectors.DOB.value;
-  DOMSelectors.form.insertAdjacentHTML("afterend", `<p>${Name}</p>`);
+  DOMSelectors.form.insertAdjacentHTML(
+    "afterend",
+    `<p>${Name} ${DOB}</p> <button type="button" id="Remove">Remove</button>`
+  );
 
   DOMSelectors.Name.value = "";
   DOMSelectors.DOB.value = "";
 });
-console.log(DOMSelectors.Name.value);
-console.log(DOMSelectors.DOB.value);
+
+DOMSelectors.Remove.addEventListener("click", function () {});
+console.log(Name);
+console.log(DOB);
+
+e.preventDefault();
