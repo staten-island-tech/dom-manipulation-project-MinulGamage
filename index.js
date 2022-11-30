cc8d203c455933d4f455468bc90aaa1c03ac16c3
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-11-30 10:13:54 -0500

    DOM

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const DOMSelectors = {
   form: document.getElementById("mainForm"),
   First: document.querySelector(`#First`),
   Last: document.querySelector(`#Last`),
+  Remove: document.getElementById("Remove"),
   DOB: document.querySelector(`#DOB`),
 };
 console.log(DOMSelectors.Name);
@@ -15,36 +16,38 @@ DOMSelectors.button.addEventListener("click", function () {
     DOB: DOMSelectors.DOB.value,
   };
 
-  const values = [{ First: "", Last: "", DOB: "" }];
+  /*   const values = [{ First: "", Last: "", DOB: "" }];
   console.log(values);
 
   values.forEach((values) => console.log(values.First));
   values.forEach((values) => console.log(values.Last));
   values.forEach((values) => console.log(values.DOB));
-
+ */
   DOMSelectors.form.insertAdjacentHTML(
     "afterend",
 
-    `<div id=parent> <p>First Name: ${info.firstname}, Last Name: ${info.lastname}, DOB: ${info.DOB}  <button type="button" id="remove">Remove</button> </p> </div> `
+    `<div class="Card" id=""Info> <p>First Name: ${info.firstname}, Last Name: ${info.lastname}, DOB: ${info.DOB}  <button type="button" id="removeBtn">Remove</button> </p> </div>`
   );
 
-  /*  let removeButton = document.getElementById("removeButton");
+  DOMSelectors.button.addEventListener("click", function clearinput() {
+    DOMSelectors.First.value = "";
+    DOMSelectors.Last.value = "";
+    DOMSelectors.DOB.value = "";
+  });
+
+  const button = document.getElementById("removeBtn");
+  document.querySelectorAll(".removeBtn");
   button.addEventListener("click", function removeCard(event) {
-    const element = document.getElementById("Remove");
-    element.remove();
-  }); */
+    const values = document.getElementById("Info");
+    event.target.remove(values);
+  });
 });
 //make object from values
 
-DOMSelectors.button.addEventListener("click", function clearinput() {
-  DOMSelectors.First.value = "";
-  DOMSelectors.Last.value = "";
-  DOMSelectors.DOB.value = "";
-});
-
-let removeButton = document.querySelectorAll(".remove");
+/* let removeButton = document.querySelectorAll(".remove");
 removeButton.forEach((button) => {
   button.addEventListener("click", function (event) {
     event.target.remove();
   });
 });
+ */
